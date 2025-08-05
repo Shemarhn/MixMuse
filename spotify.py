3554f45317bcb7929d888b0bdb732196da8d1f8f
Shemarhn <marks.shemarhn@gmail.com>
2025-08-05 13:38:29 -0500

    Added the spotify file and load environment variables

---
+++ spotify.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+
+load_dotenv()  # Load environment variables from .env file  
